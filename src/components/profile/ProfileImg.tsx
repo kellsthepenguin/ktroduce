ac993958f8f28e43a19661b250d931c678f63a62
Kim Jioo <jwkim101201@gmail.com>
2021-01-06 11:27:54 +0900

    add alt

--- src/components/profile/ProfileImg.tsx
+++ src/components/profile/ProfileImg.tsx
@@ -4,7 +4,7 @@ import '../../styles/ProfileImg.css';
 class ProfileImg extends Component {
   render () {
     return (
-      <img src="/me.gif" alt=""/>
+      <img src="/me.gif" alt="kells img"/>
     )
   }
 }
